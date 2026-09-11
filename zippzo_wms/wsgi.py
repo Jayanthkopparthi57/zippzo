bd3160839262e731ed456fa589f334b34c1c0d6c
Jay <113628769+Jayanthkopparthi57@users.noreply.github.com>
2026-09-11 20:33:53 +0530

    Seed empty deployment database at startup

--- zippzo_wms/wsgi.py
+++ zippzo_wms/wsgi.py
@@ -1,5 +1,21 @@
 import os
+import logging
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zippzo_wms.settings')
 application = get_wsgi_application()
+
+
+def ensure_seed_data():
+	"""Populate a new deployment once so the console has master data to use."""
+	try:
+		from django.core.management import call_command
+		from wms_master.models import Site
+
+		if not Site.objects.exists():
+			call_command('seed_data', verbosity=0)
+	except Exception:
+		logging.getLogger(__name__).exception('Unable to seed initial WMS data')
+
+
+ensure_seed_data()
